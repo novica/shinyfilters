31805b931ac85a57d5e481d3c0dd2d3fad4bb1f5
Josh Livingston <joshwlivingston@gmail.com>
2025-12-30 12:22:37 -0700

    test: add tests for errors thrown outside of shiny closes #66

---
+++ tests/testthat/setup_00_S7.R
@@ -0,0 +1,2 @@
+ClassCharacter <- S7::new_class("ClassCharacter", S7::class_character)
+ClassList <- S7::new_class("ClassList", S7::class_list)
---
+++ tests/testthat/test-errors.R
@@ -0,0 +1,233 @@
+# apply_filters ####
+test_that("apply_filters: unknown filter_combine_method", {
+	expect_error(
+		apply_filters(
+			test_df,
+			list(chr_col = "i"),
+			filter_combine_method = "unk"
+		),
+		'Unknown `filter_combine_method` value: unk'
+	)
+})
+
+test_that("apply_filters: filter_combine_method must be function", {
+	expect_error(
+		apply_filters(
+			test_df,
+			list(chr_col = "i"),
+			filter_combine_method = 123
+		),
+		"Argument `filter_combine_method` must be a function\\."
+	)
+})
+
+# args_filter_input ####
+## invalid args_* provided ####
+test_that("args_filter_input validates args_unique must be list", {
+	expect_error(
+		args_filter_input(choices_chr, args_unique = "not_a_list"),
+		"Supplied arguments must be a list\\."
+	)
+})
+
+test_that("args_filter_input validates args_sort must be list", {
+	expect_error(
+		args_filter_input(choices_chr, args_sort = "not_a_list"),
+		"Supplied arguments must be a list\\."
+	)
+})
+
+test_that("args_filter_input validates args_unique names are unique", {
+	lst <- list(a = 1, b = 2)
+	names(lst) <- c("a", "a")
+	expect_error(
+		args_filter_input(choices_chr, args_unique = lst),
+		"All argument names must be unique\\."
+	)
+})
+
+test_that("args_filter_input validates args_sort names are unique", {
+	lst <- list(a = 1, b = 2)
+	names(lst) <- c("a", "a")
+	expect_error(
+		args_filter_input(choices_chr, args_sort = lst),
+		"All argument names must be unique\\."
+	)
+})
+
+## invalid choices_asis provided ####
+test_that("args_filter_input: choices_asis must be TRUE for list", {
+	expect_error(
+		args_filter_input(choices_lst, choices_asis = FALSE),
+		"Argument `choices_asis` must be TRUE when `x` is a list\\."
+	)
+})
+
+## invalid extension implemented ####
+### not a list
+test_that("args_filter_input: extension does not return list", {
+	method(args_filter_input, ClassCharacter) <- function(x) "not a list"
+	expect_error(
+		filterInput(ClassCharacter(letters)),
+		"Value must be a NULL or a list."
+	)
+})
+
+### list is not named
+test_that("args_filter_input: extension does not return named list", {
+	method(args_filter_input, ClassCharacter) <- function(x) list("not named")
+	expect_error(
+		filterInput(ClassCharacter(letters)),
+		"All list elements must be named."
+	)
+
+	method(args_filter_input, ClassCharacter) <- function(x) {
+		list("not named", named = "named")
+	}
+	expect_error(
+		filterInput(ClassCharacter(letters)),
+		"All list elements must be named."
+	)
+})
+
+### list is not uniquely named
+test_that("args_filter_input: extension does not return uniquely named list", {
+	method(args_filter_input, ClassCharacter) <- function(x) {
+		list(entry = "a", entry = "b") # nolint
+	}
+	expect_error(
+		filterInput(ClassCharacter(letters)),
+		"All list names must be unique."
+	)
+})
+
+# call_filter_input ####
+test_that("call_filter_input errors for data.frames", {
+	expect_error(
+		call_filter_input(test_df, shiny::selectInput),
+		"call_filter_input\\(\\) is not implemented for data\\.frames\\."
+	)
+})
+
+# call_update_filter_input ####
+test_that("call_update_filter_input errors for data.frames", {
+	expect_error(
+		call_update_filter_input(test_df, shiny::updateSelectInput),
+		"call_update_filter_input\\(\\) is not implemented for data\\.frames\\."
+	)
+})
+
+# get_filter_logical ####
+## column nopt found
+test_that("get_filter_logical: column not found", {
+	expect_error(
+		get_filter_logical(test_df, "i", column = "nonexistent"),
+		"Column `nonexistent` not found in `x`\\."
+	)
+})
+
+## invalid column_name argument ####
+test_that("get_filter_logical: column argument is non-empty string", {
+	expect_error(
+		get_filter_logical(test_df, "i", column = NA_character_),
+		"`column` must be a non-empty string"
+	)
+
+	expect_error(
+		get_filter_logical(test_df, "i", column = ""),
+		"`column` must be a non-empty string"
+	)
+})
+
+## invalid implementation ####
+### non-logical vector returned ####
+test_that("get_filter_logical: non-logical vector returned", {
+	method(get_filter_logical, list(ClassCharacter, class_character)) <- function(
+		x,
+		val
+	) {
+		integer(length(x))
+	}
+	df <- data.frame(x = ClassCharacter(letters))
+	expect_error(
+		apply_filters(df, list(x = letters[1:5])),
+		"Filter on column `x` did not return a logical vector."
+	)
+})
+
+### logical vector of invalid length returned ####
+test_that("get_filter_logical: logical vector of invalid length", {
+	method(get_filter_logical, list(ClassCharacter, class_character)) <- function(
+		x,
+		val
+	) {
+		logical(length(x) - 1L)
+	}
+	df <- data.frame(x = ClassCharacter(letters))
+	expect_error(
+		apply_filters(df, list(x = letters[1:5])),
+		"Filter on column `x` returned a logical vector of length 25, but expected length 26."
+	)
+})
+
+# filterInput ####
+## `radio` and `selectize` both TRUE ####
+test_that("filterInput: radio and selectize cannot both be TRUE", {
+	expect_error(
+		filterInput(
+			choices_chr,
+			inputId = "test",
+			label = "Label",
+			radio = TRUE,
+			selectize = TRUE
+		),
+		"Arguments `radio` and `selectize` cannot both be TRUE\\."
+	)
+})
+
+## S7 method not found ####
+test_that("filterInput: method not found for S7 object passed as list", {
+	obj <- ClassList(as.list(letters))
+	expect_error(
+		filterInput(obj),
+		"No method found for `filterInput\\(\\)` for class `ClassList`\\."
+	)
+})
+
+# `ns` ####
+test_that("ns must be result of shiny::NS()", {
+	expect_error(
+		filterInput(
+			x = choices_chr,
+			inputId = "my_input",
+			label = "Label",
+			ns = function(x) x
+		),
+		"`ns` must be the result of calling `shiny::NS\\(\\)`"
+	)
+})
+
+test_that("ns requires inputId argument", {
+	ns <- shiny::NS("mymodule")
+	expect_error(
+		filterInput(
+			x = choices_chr,
+			label = "Label",
+			ns = ns
+		),
+		"Argument `inputId` is required when `ns` is provided"
+	)
+})
+
+# updateFilterInput ####
+test_that("updateFilterInput: radio and selectize cannot both be TRUE", {
+	expect_error(
+		updateFilterInput(
+			choices_chr,
+			inputId = "test",
+			radio = TRUE,
+			selectize = TRUE
+		),
+		"Arguments `radio` and `selectize` cannot both be TRUE\\."
+	)
+})
